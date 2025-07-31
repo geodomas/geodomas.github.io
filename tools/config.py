eda5b9592acdb47b22c7e06903d8581185a91ab7
SOPHYA Q METACORE <geodomasdi@gmail.com>
2025-07-31 23:15:08 +0300

    Update config.py

--- tools/config.py
+++ tools/config.py
@@ -1,5 +1,5 @@
 import os
 
-FIRMWARE_FILE = os.path.join("MetaCore_FIRMWARE", "core", "firmware_qna_test.md")
+FIRMWARE_FILE = os.path.join("MetaCore_FIRMWARE", "core", "firmware_qna_test.docx")
 FIRMWARE_INDEX_PATH = os.path.join("MetaCore_FIRMWARE", "config", "firmware_index.json")
 OUTPUT_FILE = os.path.join("demo", "result_output.json")
